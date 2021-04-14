9644a45177976bac6c87fcefb839390667edacd0
Ayushman Dixit <46623283+ayushman2001@users.noreply.github.com>
2021-04-14 15:40:35 +0530

    Add files via upload

---
+++ Module2_ServerClient/Client.py
@@ -0,0 +1,33 @@
+import socket
+import tqdm
+import os
+
+def send_file(s , filename):
+    filesize = os.path.getsize(filename)
+    s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                
+                break
+            
+            s.sendall(bytes_read)
+            
+            progress.update(len(bytes_read))
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096 
+
+changes = ["./newf/tempFile"]
+host = "127.0.0.1"
+port = 5001
+s = socket.socket()
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected.")
+for items in changes:
+    send_file(s,items)
+s.close()
