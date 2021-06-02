464ac85fd3b80015c165ff882bcfd81559e3cbb4
nidhijainpnp <12njain06@gmail.com>
2021-06-02 12:18:01 +0530

    enabled multiclient

--- Module2_ServerClient/Client.py
+++ Module2_ServerClient/Client.py
@@ -1,7 +1,11 @@
 import socket
 import tqdm
 import os
+from concurrent.futures import ThreadPoolExecutor
 
+from queue import Queue
+from _thread import *
+import threading
 def send_file(s , filename):
     filesize = os.path.getsize(filename)
     s.send(f"{filename}{SEPARATOR}{filesize}".encode())
@@ -21,7 +25,9 @@ def send_file(s , filename):
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096 
 
-changes = ["./newf/tempFile"]
+changes = []
+input_filename = input("Enter filename:")
+changes.append(input_filename)
 host = "127.0.0.1"
 port = 5001
 s = socket.socket()
--- Module2_ServerClient/server.py
+++ Module2_ServerClient/server.py
@@ -1,6 +1,8 @@
 import socket
 import tqdm
 import os
+from _thread import *
+import threading
 
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 5001
@@ -14,32 +16,40 @@ s = socket.socket()
 s.bind((SERVER_HOST, SERVER_PORT))
 s.listen(5)
 print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-client_socket, address = s.accept() 
 
-print(f"[+] {address} is connected.")
+def func(client_socket):
+    
+    received = client_socket.recv(BUFFER_SIZE).decode()
+    print(received,flush=True)
+    filename, filesize = received.split(SEPARATOR)
 
-received = client_socket.recv(BUFFER_SIZE).decode()
-filename, filesize = received.split(SEPARATOR)
+    filename = os.path.basename(filename)
 
-filename = os.path.basename(filename)
-
-filesize = int(filesize)
-
-
-progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-with open("hello", "wb") as f:
-    while True:
-        
-        bytes_read = client_socket.recv(BUFFER_SIZE)
-        if not bytes_read:    
-
-            break
-        
-        f.write(bytes_read)
-        
-        progress.update(len(bytes_read))
+    filesize = int(filesize)
 
 
-client_socket.close()
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open("hello_", "wb") as f:
+        while True:
+                
+            bytes_read = client_socket.recv(BUFFER_SIZE)
+            if not bytes_read:    
+                    break
+                
+            f.write(bytes_read)
+                
+            progress.update(len(bytes_read))
 
+    #print("yup",flush =True)
+    client_socket.close()
+
+    #s.close()
+tmp=0
+while (1):
+    client_socket, address = s.accept() 
+
+    print(f"[+] {address} is connected.")
+    start_new_thread(func, (client_socket,))
 s.close()
+    
+
